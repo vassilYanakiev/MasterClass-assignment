f58f97d61c5a09da65bc9cf1e9421760f023b9d5
Vassil Yanakiev <vsy4@mail.bg>
2019-11-14 15:45:42 +0200

    Updates

---
+++ precache-manifest.27da7d7024cebc5b41b172b749446110.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4dc511bc02f599a0455f9973f1b550b",
+    "url": "/MasterClass-assignment/index.html"
+  },
+  {
+    "revision": "40e67ba54ddc47e3b51c",
+    "url": "/MasterClass-assignment/static/css/main.862be62a.chunk.css"
+  },
+  {
+    "revision": "a543cb116a9a75406cfb",
+    "url": "/MasterClass-assignment/static/js/2.0d660175.chunk.js"
+  },
+  {
+    "revision": "40e67ba54ddc47e3b51c",
+    "url": "/MasterClass-assignment/static/js/main.86e3e1e6.chunk.js"
+  },
+  {
+    "revision": "9c8b6a0d303e2cfd9a1c",
+    "url": "/MasterClass-assignment/static/js/runtime-main.26bbad46.js"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/MasterClass-assignment/static/media/STARWARS.3f085be9.ttf"
+  },
+  {
+    "revision": "9652b3f052466a96ebad3c42f6812ce0",
+    "url": "/MasterClass-assignment/static/media/SfDistantGalaxyOutline-xoeO.9652b3f0.ttf"
+  },
+  {
+    "revision": "c4a635f68b6f318fc2caec12833b5967",
+    "url": "/MasterClass-assignment/static/media/logout.c4a635f6.svg"
+  },
+  {
+    "revision": "9b14fc5220c2547e5c44408f1915d121",
+    "url": "/MasterClass-assignment/static/media/logout2.9b14fc52.svg"
+  }
+]);
